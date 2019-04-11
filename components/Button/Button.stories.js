23c81089afac6e322ef3289f4a7d144fd0edcb5f
Eevee Kreevee <thekrambuhl@gmail.com>
2019-04-10 17:34:25 -0700

    fix stories

--- components/Button/Button.stories.js
+++ components/Button/Button.stories.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { action } from '@storybook/addon-actions'
 import { Button } from 'Components'
 import Centered from 'Decorators/Centered'
 
 storiesOf('Button', module)
   .addDecorator(Centered)
-  .add('with text', () => <Button onClick={action('clicked')}>Send</Button>)
+  .add('with text', () => <Button>Send</Button>)
   .add('with link', () => <Button href="#/">Link</Button>)
   .add('filled variant', () => <Button variant="filled">Filled Button</Button>)
   .add('loading state', () => <Button variant="filled" isLoading>Loading Button</Button>)
