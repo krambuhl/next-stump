a09f6e1177730b8d852010cd5564ba4244b3c68e
Eevee Kreevee <thekrambuhl@gmail.com>
2019-04-10 17:35:34 -0700

    codestyle

--- components/ProjectGallery/ProjectGallery.js
+++ components/ProjectGallery/ProjectGallery.js
@@ -1,28 +1,25 @@
 import React from 'react'
 import styles from './styles.css'
 
-const ProjectGallery = ({ project }) => {
-  console.log(project)
-  return (
-    <div className={styles.root}>
-      {project.projectImages.map(({ alt, image }) => (
-        <div key={alt} className={styles.container}>
-          <div
-            className={styles.image}
-            style={{ paddingBottom: `${image.height / image.width * 100}%` }}
-          >
-            <img
-              src={image.src}
-              srcSet={image.srcSet}
-              alt={alt}
-            />
-          </div>
-
-          <p className={styles.label}>{alt}</p>
+const ProjectGallery = ({ project }) => (
+  <div className={styles.root}>
+    {project.projectImages.map(({ alt, image }) => (
+      <div key={alt} className={styles.container}>
+        <div
+          className={styles.image}
+          style={{ paddingBottom: `${image.height / image.width * 100}%` }}
+        >
+          <img
+            src={image.src}
+            srcSet={image.srcSet}
+            alt={alt}
+          />
         </div>
-      ))}
-    </div>
-  )
-}
+
+        <p className={styles.label}>{alt}</p>
+      </div>
+    ))}
+  </div>
+)
 
 export default ProjectGallery
